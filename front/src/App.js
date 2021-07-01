c3fe3041d8041b321623ac6638e2614b3596094f
Sergio Silva <sergiobtos@hotmail.com>
2021-07-01 17:06:28 -0400

    Adding a Main Header

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
-  return <Router>
-    <Switch>
-    <Route path="/" exact> <Users/> </Route>
-    <Route path="/places/new" exact> <NewPlace/> </Route>
-    <Redirect to="/" />
-    </Switch>
-  </Router>;
-};
+  return (
+  <Router>
+    <MainNavigation/>
+    <main>
+      <Switch>
+      <Route path="/" exact> <Users/> </Route>
+      <Route path="/places/new" exact> <NewPlace/> </Route>
+      <Redirect to="/" />
+      </Switch>
+    </main>
+  </Router>
+  )};
 
 export default App;
