2dff19c0fe4fad5b885b376687f48116728a0afd
Sergio Silva <sergiobtos@hotmail.com>
2021-07-02 11:58:16 -0400

    Animating the sidedrawer

--- front/src/shared/components/Navigation/MainNavigation.js
+++ front/src/shared/components/Navigation/MainNavigation.js
@@ -1,15 +1,33 @@
 //imr
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
+    const[drawerIsOpen, setDrawerIsOpen] = useState(false);
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);        
+    }
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    }
     return (
+        <React.Fragment>
+        {drawerIsOpen && <Backdrop onClick={closeDrawerHandler}/>}
+        
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+            <nav className="main-navigation__drawer-nav">
+                <NavLinks />
+            </nav>
+        </SideDrawer>
     <MainHeader>
-       <button className="main-navigation__menu-btn">
+       <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
            <span/>
            <span/>
            <span/>
@@ -17,8 +35,9 @@ const MainNavigation = props => {
         <h1 className="main-navigation__title">
             <Link to="/">Your Places</Link>
         </h1>
-        <nav><NavLinks/></nav>
+        <nav className="main-navigation__header-nav"><NavLinks/></nav>
     </MainHeader>
+    </React.Fragment>
 )};
 
 export default  MainNavigation;
