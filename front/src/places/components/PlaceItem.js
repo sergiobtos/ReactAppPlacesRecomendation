2b4637d4cab467692b408698eb3d81f45a5c09b4
Sergio Silva <sergiobtos@hotmail.com>
2021-07-04 22:33:09 -0400

    Adding Input Validation

--- front/src/places/components/PlaceItem.js
+++ front/src/places/components/PlaceItem.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Buttons';
+import Modal from '../../shared/components/UIElements/Modal';
+import Map from '../../shared/components/UIElements/Map';
 import './PlaceItem.css';
 
 const PlaceItem = props => {
+    const [showMap, setShowMap] = useState(false);
+
+    const openMapHandler = () => setShowMap(true);
+
+    const closeMapHandler = () => setShowMap(false);
+
     return(
+        <React.Fragment>
+            <Modal 
+            show={showMap} 
+            onCancel={closeMapHandler} 
+            header={props.address} 
+            contentClass="place-item__modal-content"
+            footerClass="place-item__modal-actions"
+            footer={<Button onClick={closeMapHandler}>Close</Button>}
+            >
+            <div className="map-container"><Map center={props.coordinates} zoom={16}/></div>  
+            </Modal>
         <li className="place-item">
             <Card className="place-item__content">
             <div className="place-item__image">
@@ -17,12 +36,13 @@ const PlaceItem = props => {
                 <p>{props.description}</p>
             </div>
             <div className="place-item__action">
-                <Button inverse>View on Map</Button>
+                <Button inverse onClick={openMapHandler}>View on Map</Button>
                 <Button to={`/places/${props.id}`}>Edit</Button>
                 <Button danger >Delete</Button>
             </div>
             </Card>
         </li>
+        </React.Fragment>
     );
 };
 
