af338d1e3c3ae66bb87d8b908d5df8b55d8c7faf
Sergio Silva <sergiobtos@hotmail.com>
2021-07-11 22:54:43 -0400

    First Commit

---
+++ backend/routes/places-routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const HttpError = require('../models/http-error');
+const router = express.Router();
+
+const DUMMY = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th St, New York, NY 10001',
+        creator: 'u1'
+    }
+
+];
+
+router.get('/:pid', (req, res, next)=>{
+    const placeId = req.params.pid;
+    const place = DUMMY.find(p => {return p.id === placeId});
+    if(!place){
+        return next(new HttpError( 'Could not find a place for the provided place id.', 404));
+    }
+    res.json({place});
+});
+
+router.get('/user/:uid', (req, res, next)=>{
+    const userId = req.params.uid;
+    const place = DUMMY.find(p => {return p.creator === userId});
+    if(!place){
+        return next(new HttpError( 'Could not find a place for the provided user id.', 404));
+    }
+    res.json({place});
+});
+
+
+module.exports = router;
